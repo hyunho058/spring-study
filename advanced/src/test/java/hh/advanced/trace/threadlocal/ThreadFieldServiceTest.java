a73644e80d1caf2f16d4169168dfc98756413b11
hyunho058 <hyunho058@naver.com>
2021-11-17 19:55:38 +0900

    [Spring_advanced] Test ThreadLocal

--- advanced/src/test/java/hh/advanced/trace/threadlocal/ThreadFieldServiceTest.java
+++ advanced/src/test/java/hh/advanced/trace/threadlocal/ThreadFieldServiceTest.java
@@ -1,22 +1,22 @@
 package hh.advanced.trace.threadlocal;
 
-import hh.advanced.trace.threadlocal.code.FieldService;
+import hh.advanced.trace.threadlocal.code.ThreadLocalFieldService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
-public class FieldServiceTest {
+public class ThreadFieldServiceTest {
 
-    private FieldService fieldService = new FieldService();
+    private ThreadLocalFieldService service = new ThreadLocalFieldService();
 
     @Test
     void field() {
         log.info("main start");
         Runnable userA = () -> {
-            fieldService.logic("userA");
+            service.logic("userA");
         };
         Runnable userB = () -> {
-            fieldService.logic("userB");
+            service.logic("userB");
         };
 
         Thread threadA = new Thread(userA);
@@ -25,8 +25,7 @@ public class FieldServiceTest {
         threadB.setName("thread-B");
 
         threadA.start();
-//        sleep(2000);//동시성 문제 발생X
-        sleep(100);//동시성 문제 발생
+        sleep(100);
         threadB.start();
         //countdown letgi
         sleep(3000);//메인 쓰레드 종료 대기
