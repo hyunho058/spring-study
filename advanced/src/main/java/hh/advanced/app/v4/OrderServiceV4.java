504a7b741774f2bbf7e998968a7090694caf8870
hyunho058 <hyunho058@naver.com>
2021-11-18 18:17:22 +0900

    [Spring_advanced] Add v4 log trace using template method pattern

---
+++ advanced/src/main/java/hh/advanced/app/v4/OrderServiceV4.java
@@ -0,0 +1,26 @@
+package hh.advanced.app.v4;
+
+import hh.advanced.trace.TraceStatus;
+import hh.advanced.trace.logtrace.LogTrace;
+import hh.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV4 {
+
+    private final OrderRepositoryV4 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId) {
+        AbstractTemplate<Void> template = new AbstractTemplate<Void>(trace) {
+            @Override
+            protected Void call() {
+                orderRepository.save(itemId);
+                return null;
+            }
+        };
+        template.execute("OrderService.orderItem()");
+    }
+}
