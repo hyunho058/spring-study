d0528aa13aef1eddce05a8001df951a43e1a1c6b
hyunho058 <hyunho058@naver.com>
2022-05-17 22:40:42 +0900

    [Spring_DB] TransactionTemplate 사용하여 commit, rollback 중복 코드 제거

---
+++ database/src/main/java/hh/database/jdbc/service/MemberServiceV3_2.java
@@ -0,0 +1,53 @@
+package hh.database.jdbc.service;
+
+import hh.database.jdbc.domain.Member;
+import hh.database.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.SQLException;
+
+/**
+ * Transaction - 트랜잭션 탬플릿
+ */
+@Slf4j
+public class MemberServiceV3_2 {
+
+    private final TransactionTemplate transactionTemplate;
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.transactionTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
+
+    public void accountTransfer(String fromId, String toId, int money) {
+        transactionTemplate.executeWithoutResult(transactionStatus -> {
+            //비즈니스 로직
+            try {
+                bizLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validateion(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validateion(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
